daf94665a8b621beb31d6dabe1e67c9e07bb958b
mpixton <71086131+mpixton@users.noreply.github.com>
2021-03-20 14:40:04 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MovieCollectionRevisited.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

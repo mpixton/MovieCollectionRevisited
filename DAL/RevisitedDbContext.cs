6ce9927d7edb55234dc76f7ccd4f9273c09dcbfe
mpixton <71086131+mpixton@users.noreply.github.com>
2021-03-20 15:49:19 -0600

    FEAT: create db connection
    Added the DbContext class for the project. Configured the project to use SqLite. Added the connection string to the appsettings.json file. Added the DbContext to the Startup ConfigureServices call.

---
+++ DAL/RevisitedDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieCollectionRevisited.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieCollectionRevisited.DAL
+{
+    public class RevisitedDbContext : DbContext
+    {
+        public RevisitedDbContext(DbContextOptions<RevisitedDbContext> options) : base (options)
+        { }
+
+        /// <summary>
+        /// Loads all Movies from the Database into memory for server processing.
+        /// </summary>
+        public DbSet<Movie> Movies { get; set; }
+    }
+}

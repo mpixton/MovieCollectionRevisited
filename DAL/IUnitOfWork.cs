58f5caba5c392316f2c2bb1dd992f69b80bcaf01
mpixton <71086131+mpixton@users.noreply.github.com>
2021-03-20 21:27:53 -0600

    DOC: data access layer
    Added XML comments to Unit Of Work related class and interface.

--- DAL/IUnitOfWork.cs
+++ DAL/IUnitOfWork.cs
@@ -2,10 +2,19 @@
 
 namespace MovieCollectionRevisited.DAL
 {
+    /// <summary>
+    /// Defines the members for the Unit Of Work class. 
+    /// </summary>
     public interface IUnitOfWork
     {
+        /// <summary>
+        /// Generic Repo of Movie objects.
+        /// </summary>
         GenericRepo<Movie> MovieRepo { get; }
 
+        /// <summary>
+        /// Saves the changes made to the Repos in this Unit Of Work to the Db.
+        /// </summary>
         void Save();
     }
 }

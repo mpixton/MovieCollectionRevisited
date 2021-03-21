5ac6b83f20fdbf5ac189d6175f9af7db3ed3e231
mpixton <71086131+mpixton@users.noreply.github.com>
2021-03-20 21:27:53 -0600

    DOC: data access layer
    Added XML comments to Unit Of Work related class and interface.

--- DAL/UnitOfWork.cs
+++ DAL/UnitOfWork.cs
@@ -2,9 +2,19 @@
 
 namespace MovieCollectionRevisited.DAL
 {
+    /// <summary>
+    /// Unit Of Work class. Implements the IUnitOfWork members.
+    /// </summary>
     public class UnitOfWork : IUnitOfWork
     {
+        /// <summary>
+        /// DbContext to be shared by all Repos in this Unit Of Work. Prevents concurrency issues.
+        /// </summary>
         private RevisitedDbContext _context;
+
+        /// <summary>
+        /// Repo for interacting with all Movie objects.
+        /// </summary>
         private GenericRepo<Movie> _movieRepo;
 
         public UnitOfWork(RevisitedDbContext context)
@@ -12,6 +22,11 @@ namespace MovieCollectionRevisited.DAL
             _context = context;
         }
 
+        /// <summary>
+        /// If MovieRepo Doesn't exists, create a new one with the DbContext provided by dependency 
+        /// injection. If already exists, return the current one. Ensures that all Repos are sharing
+        /// the same DbContext so that changes to mulitple Repos are saved at the same time.
+        /// </summary>
         public GenericRepo<Movie> MovieRepo
         {
             get
@@ -25,7 +40,9 @@ namespace MovieCollectionRevisited.DAL
             }
         }
 
-
+        /// <summary>
+        /// Saves the chagnes across all Repos in one go.
+        /// </summary>
         public void Save()
         {
             _context.SaveChanges();
